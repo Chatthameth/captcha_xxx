7aa0e56500382ff4e69bbf7e4d242897ea2c4c65
Chatthameth <phaky.chatthameth@gmail.com>
2016-10-14 12:11:12 +0700

    edit

--- spec/captcha.spec.js
+++ spec/captcha.spec.js
@@ -1,6 +1,6 @@
 function captcha(pattern,operator,Loperand,Roperand){
   this.generate = function(){
-    return new L.toString(pattern,Loperand) + ' ' + O.toString(operator) + ' ' + R.toString(pattern,Roperand);
+    return new (L.toString(pattern,Loperand) + ' ' + O.toString(operator) + ' ' + R.toString(pattern,Roperand));
   }
 }
 let num = [0,1,2,3,4,5,6,7,8,9];
@@ -19,5 +19,5 @@ function R(pattern,Roperand){
 }
 
 describe('captcha app', () => {
-  
+
 })

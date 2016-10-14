63e4878fee1acf66e0b704b781017b17e030e8cd
Chatthameth <phaky.chatthameth@gmail.com>
2016-10-14 12:35:16 +0700

    แก้ไขใน generate นิดนึง

--- spec/captcha.spec.js
+++ spec/captcha.spec.js
@@ -1,6 +1,6 @@
 function captcha(pattern,operator,Loperand,Roperand){
   this.generate = function(){
-    return new (L.toString(pattern,Loperand) + ' ' + O.toString(operator) + ' ' + R.toString(pattern,Roperand));
+    return new (L(pattern,Loperand) + ' ' + O(operator) + ' ' + R(pattern,Roperand));
   }
 }
 let num = [0,1,2,3,4,5,6,7,8,9];

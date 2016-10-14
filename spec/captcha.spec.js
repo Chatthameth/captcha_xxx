5268ea2721cbc1e5b6277a6a04154d03c8257905
Chatthameth <phaky.chatthameth@gmail.com>
2016-10-14 12:30:03 +0700

    เพิ่มเงื่อนไขในฟังชั่น Operator

--- spec/captcha.spec.js
+++ spec/captcha.spec.js
@@ -7,8 +7,10 @@ let num = [0,1,2,3,4,5,6,7,8,9];
 let text = ['ZERO','ONE','TWO','THREE','FOUR','FIVE','SIX','SEVEN','EIGHT','NINE'];
 
 function O(operator){
-
-}
+this.toString = function(){
+  if(operator === 1) return '+';
+  else if(operator === 2) return '-';
+}}
 
 function L(pattern,Loperand){
 
